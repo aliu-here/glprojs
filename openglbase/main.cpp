1e9fb2b3e779f24f31a4afa2eded129cdf51236d
Andrew Liu <andrew.yc.liu@gmail.com>
2023-09-02 14:36:25 -0700

    adding textures, 3d stuff, and movement to the "base" project

--- openglbase/main.cpp
+++ openglbase/main.cpp
@@ -1,6 +1,7 @@
 #include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 //textures
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 //glm
 #include <glm/glm.hpp>
@@ -11,26 +12,65 @@
 
 #include <iostream>
 #include "include/shader.h"
+#include "process_input.cpp"
+
+callback_funcs camera(90.0f, 145.0f);
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+float lastScrollFrame = lastFrame;
+
+//camera
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+glm::vec3 cameraUD    = glm::cross(cameraFront, cameraUp);
+
+//global values for the callback functions
+float fov;
+float lastX = 1920.0f/2, lastY = 1080.0f/2;
+float pitch=0, yaw=-90.0f;
+bool firstMouse = true, mouseLocked = true;
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
-	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+	if (camera.mouseLocked){
+		camera.process_mouse_move(window, (float)xposIn, (float)yposIn);
+	}
 }
 
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.process_mouse_scroll(window, xoffset, yoffset, lastFrame);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    camera.process_mouse_button(window, button, action, mods);
+}
 int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	float origfov;
+	std::cout << "FOV: ";
+	std::cin >> origfov;
+
+	fov = origfov;
+
+	//stupid but we need the camera to be global, so we initialize it with a constant and then redefine it
+	camera.update_fov(fov);
 	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -45,40 +85,144 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, 1920, 1080);
 
-	float vertices[] = {         
-	0.5f,  0.5f, 0.0f,  
-	0.5f, -0.5f, 0.0f, 
-	-0.5f, -0.5f, 0.0f };
+	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	camera.init_dims(mode->width, mode->height);
 
+	//lock cursor to center
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
+
+	int width,height, nrChannels;
+	unsigned char *data = stbi_load("resources/rubberroom.jpg", &width, &height, &nrChannels, 0);
+	std::cout << "width: " << width << " height: " << height << '\n' << " nrchannels: " << nrChannels << '\n';
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(data);
+
+float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};	
 	unsigned int VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+//	glGenBuffers(1, &EBO);
 	
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	Shader ourShader("shaders/vertex.glsl", "shaders/fragment.glsl");
 	
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+	glfwSetScrollCallback(window, scroll_callback); 
+
+	glEnable(GL_DEPTH_TEST);  	
 	while(!glfwWindowShouldClose(window))
 	{
-		processInput(window);
+
+		float currentFrame = static_cast<float>(glfwGetTime());
+        	deltaTime = currentFrame - lastFrame;
+        	lastFrame = currentFrame;
+
+		camera.process_kb_input(window, deltaTime, currentFrame);
+
+		if (camera.fov != origfov && currentFrame - camera.lastScrollFrame >= 0.25f)
+		{
+			camera.update_fov((origfov - camera.fov) / 60 + camera.fov);
+		}
 
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // create transformations
+        	glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        	glm::mat4 view          = glm::mat4(1.0f);
+        	glm::mat4 projection    = glm::mat4(1.0f);
+
+		view = glm::lookAt(camera.cameraPos, camera.cameraPos + camera.cameraFront, camera.cameraUp);
+		projection = glm::perspective(glm::radians(camera.fov), 16.0f/9.0f , 0.1f, 100.0f);
+        	// retrieve the matrix uniform locations
+        	unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        	unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
+        	// pass them to the shaders (3 different ways)
+        	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+
+        	// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        	ourShader.setMat4("projection", projection);
+		ourShader.setMat4("model", model);
 
 		ourShader.use();
 
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
