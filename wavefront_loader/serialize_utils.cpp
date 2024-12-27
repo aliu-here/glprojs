bcd163c17e38bdcaa1be8295753fa652430c3d0a
aliu <andrew.yc.liu@gmail.com>
2024-12-26 19:14:36 -0800

    moved; also proper support for denormals

--- wavefront_loader/serialize_utils.cpp
+++ wavefront_loader/serialize_utils.cpp
@@ -12,15 +12,23 @@ namespace loader {
         return std::endian::native == std::endian::little;
     }
 
-    uint32_t float_to_ieee_754(float value) 
+    uint32_t float_to_ieee_754(float value)  // there's std::float32_t but that's not supported by clang++ (as of now, 12/26/24)
     {
+        if (value != value)
+            return 0x7f800001; //NaN checking
+        if (std::isinf(value)) {
+            if (value < 0)
+                return 0xff800000; //pos inf
+            else
+                return 0x7f800000; // neg inf
+        }
         const uint32_t sign_shift = 31, exp_shift = 23;
         uint32_t ieee_754_fmt = 0;
         ieee_754_fmt |= (value < 0) << sign_shift;
         if (value == 0)
             return ieee_754_fmt; // only need sign
         double dbl_exp = std::log2(std::abs(value));
-        int exp = std::round(dbl_exp);
+        int exp = dbl_exp; //truncate so no negatives
         unsigned char exp_8b;
         if (exp < -126) {
             exp_8b = 0;
@@ -32,26 +40,32 @@ namespace loader {
 
         ieee_754_fmt |= exp_8b << exp_shift;
 
-        double significand = value / (std::exp2(exp)) - 1;
-        uint32_t shifted_significand = roundeven(significand * std::exp2(23)); //ieee compliance
+        double significand;
+        if (exp_8b == 0)
+            significand = std::abs((double)value) / (std::exp2(-126)); // check for subnormals
+        else
+            significand = std::abs((double)value) / (std::exp2(exp_8b - 127)) - 1;
+        uint32_t shifted_significand = significand * std::exp2(23);
+        std::cout << exp << '\n';
+        std::cout << significand << '\n';
         ieee_754_fmt |= shifted_significand;
         return ieee_754_fmt;
     }
 
     float ieee_754_to_float(uint32_t ieee_754_fmt)
     {
-        const uint32_t significand_bitmask = 0x007fffff, exp_bitmask = 0x7f800000, exp_bitmask_shift = 23, sign_shift = 31;
+        const uint32_t significand_bitmask = 0x007fffff, exp_bitmask = 0x7f800000, exp_shift = 23, sign_shift = 31;
         float out = 0;
         uint32_t significand = (ieee_754_fmt & significand_bitmask);
-        out = significand / (std::exp2(23)) + 1;
-        unsigned char exp = (ieee_754_fmt & exp_bitmask) >> exp_bitmask_shift;
-        std::cout << "exp: " << ((int)exp) - 127 << '\n';
+        out = ((double)significand * (std::exp2(-23))) + 1;
+        unsigned char exp = (ieee_754_fmt & exp_bitmask) >> exp_shift;
+        if (exp == 0) {
+            exp = 1; //subnormals
+            out -= 1; //subnormals don't have addition here
+        }
         bool sign = ieee_754_fmt >> sign_shift;
-        std::cout << "sign: " << sign << '\n';
         if (exp == 0 && significand == 0) { // signed zeroes
             return (sign) ? -0 : +0;
-        } else if (exp == 0) { // subnormals
-            return std::exp2(exp - 127) * significand;
         }
         if (exp == 255) {
             if (significand == 0)
